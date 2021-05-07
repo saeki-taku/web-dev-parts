6ef00bf6213c511952217e65e17f584560ba21cc
Taku <63911366+saeki-taku@users.noreply.github.com>
2021-05-08 00:35:17 +0900

    preparation

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(".test").css("color", "red");
+
+// const init = () => {
+//   console.log(hello('Bob', 'Tom'));
+// };
+
+// function hello(...args) {
+//   return args.reduce((accu, curr) => `Helo Helo! ${accu} ${curr}`);
+// }
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   init();
+// });
